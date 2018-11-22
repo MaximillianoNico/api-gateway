43daa359fedae95e199337f5b1e6178de863cae0
max <max@administrators-Mac-mini-2.local>
2018-11-22 18:29:56 +0700

    initialized

---
+++ services/product.service.js
@@ -0,0 +1,91 @@
+"use strict";
+
+const Axios = require('axios');
+
+module.exports = {
+	name: "product",
+
+	/**
+	 * Service settings
+	 */
+	settings: {
+
+	},
+
+	/**
+	 * Service dependencies
+	 */
+	dependencies: [],	
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+		/**
+		 * Say a 'Hello'
+		 *
+		 * @returns
+		 */
+		get() {
+			var respon ;
+			return Axios.get('http://localhost:8080/v1/products/all')
+				.then(res => {
+					// console.log(res.data.data)
+					return res.data.data
+				})
+				.catch((err) => {
+					console.log(err)
+				})
+		},
+
+		/**
+		 * Welcome a username
+		 *
+		 * @param {String} name - User name
+		 */
+		welcome: {
+			params: {
+				name: "string"
+			},
+			handler(ctx) {
+				return `Product name, ${ctx.params.name}`;
+			}
+		}
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	started() {
+
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	stopped() {
+
+	}
+};
